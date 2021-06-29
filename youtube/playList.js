2d4a8c3f820809e0baa54f0611c5f1bc92868c91
kent199944 <mamy556618@gmail.com>
2021-06-29 23:43:12 +0800

    initial commit

---
+++ youtube/playList.js
@@ -0,0 +1,12 @@
+//YouTube Video ID
+let playList = [
+    "SwZMS-ISbH4",
+    "-mJ7sxF92YQ",
+    "c6gi6IP6ay4"
+];
+//播放起訖秒數
+let playTime = [
+    [101, 109],
+    [102, 110],
+    [146, 152]
+];
